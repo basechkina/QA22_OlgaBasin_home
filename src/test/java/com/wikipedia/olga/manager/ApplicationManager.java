e712aaf9235f0e67a74905a97ca5fc22526ed340
olgabasin <olgabasin94@gmail.com>
2020-01-30 13:41:06 +0200

    added EventFiringWebDriver

--- src/test/java/com/wikipedia/olga/manager/ApplicationManager.java
+++ src/test/java/com/wikipedia/olga/manager/ApplicationManager.java
@@ -1,30 +1,62 @@
 package com.wikipedia.olga.manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager  {
 
-    WebDriver wd;
+    EventFiringWebDriver wd;
 
     SessionHelper sessionHelper;
     ArticleHelper articleHelper;
+
+    public static class MyListener extends AbstractWebDriverEventListener{
+
+        public MyListener() {
+            super();
+        }
+
+        Logger logger = LoggerFactory.getLogger(MyListener.class);
+
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            logger.info("Search element " +by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            logger.info(by + " found!");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            logger.error("\nERROR" +throwable);
+        }
+    }
 
     public void init() {
         String browser = System.getProperty("browser", BrowserType.CHROME);
 
         if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
+            wd = new EventFiringWebDriver(new ChromeDriver());
         } else if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
+            wd = new EventFiringWebDriver(new FirefoxDriver());
         } else if (browser.equals(BrowserType.SAFARI)) {
-            wd = new SafariDriver();
+            wd = new EventFiringWebDriver(new SafariDriver());
         }
+
+        wd.register(new MyListener());
 
         wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wd.get("https://www.wikipedia.org/");
